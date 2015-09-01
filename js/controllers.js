46cfc4d742ac186065a5958fa9d7196e1a0e2012
Javier <javier@bitpay.com>
2015-09-01 16:39:10 -0300

    fix ident

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
-var app = angular.module("addressApp",[]);
-app.controller("addressController",function($scope){
+var app = angular.module("addressGeneratorApp",["addressGeneratorApp.services"]);
+app.controller("addressGeneratorController",function($scope, generatorServices){
 	$scope.generate = function(){
 		var bck = $scope.bck;
 		var pwd = $scope.pwd;
 		var validation = generatorServices.validate(bck,pwd);
-		
+
 		if(validation == true){
 			var xPrivKey = generatorServices.getXPrivKey(pwd,bck);
 			var addr = generatorServices.getAddress(xPrivKey);
