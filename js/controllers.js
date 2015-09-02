514add837bff24a6e9a1535851dd58111fc2d9c3
Javier <javier@bitpay.com>
2015-09-02 17:31:30 -0300

    adding main and change addresses

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,34 @@
 var app = angular.module("addressGeneratorApp",["addressGeneratorApp.services"]);
 app.controller("addressGeneratorController",function($scope, generatorServices){
 	$scope.generate = function(){
-		var bck = $scope.bck;
-		var pwd = $scope.pwd;
-		$scope.textbox = [];
-		var validation = generatorServices.validate(bck,pwd);
+		var backUp = $scope.backUp;
+		var password = $scope.password;
+		var validation = generatorServices.validate(backUp, password);
+		var mainPath = "m/45'/2147483647/0/";
+		var changePath = "m/45'/2147483647/1/";
+		$scope.textArea = "";
+		
+		$scope.textArea = 'Searching addresses...' + '\n';
+
 		if(validation == true){
-		generatorServices.getActiveAddresses(pwd,bck,function(addrs) {
-				$scope.textbox = addrs;
-		})
+			// getting main addresses
+			generatorServices.getActiveAddresses(password, backUp, mainPath, function(mainAddresses) {
+				if(mainAddresses.length > 0)
+					$scope.textArea += 'Main addresses:\n' + mainAddresses.toString().replace(/,/g,'\n') + '\n\n';
+				else
+					$scope.textArea += 'No Main addresses available.' + '\n';
+			});
+
+			// gettin change addresses
+			generatorServices.getActiveAddresses(password, backUp, changePath, function(changeAddresses) {
+				if(changeAddresses.length > 0)
+					$scope.textArea += 'Change addresses:\n' + changeAddresses + '\n\n';
+				else
+					$scope.textArea += 'No CHange addresses available.' + '\n';
+			});
 		}
 		else{
-			$scope.textbox= validation;
+			$scope.textArea = validation;
 		}
 	}
 });
--- js/services.js
+++ js/services.js
@@ -1,96 +1,83 @@
 var app = angular.module("addressGeneratorApp.services",[]);
 app.service('generatorServices',['$http',function($http){
 var i = 0;
-var GAP=20;
-var root={};
+var GAP = 20;
+var root = {};
 
-	root.validate = function(bck,pwd){
-		if (bck == "" || pwd == "") {
+	root.validate = function(backUp, password){
+		if (backUp == "" || password == "") {
 			return "Please Enter values for all entry boxes.";
 		};
 		try {
-			jQuery.parseJSON( bck );
+			jQuery.parseJSON( backUp );
 		} catch(e) {
 			return "Your JSON is not valid, please copy only the text within (and including) the { } brackets around it.";
 		};
 		var plain;
 		try {
-			plain = sjcl.decrypt(pwd, bck);
+			plain = sjcl.decrypt(password, backUp);
 		} catch(e) {
 			return "Seems like your password is incorrect. Try again.";
 		};
 		return true;
 	};
 
-	root.getXPrivKey = function(pwd,bck){
-		return JSON.parse(sjcl.decrypt(pwd,bck).toString()).xPrivKey;
+	root.getXPrivKey = function(password, backUp){
+		return JSON.parse(sjcl.decrypt(password, backUp).toString()).xPrivKey;
 	};
 
-	root.getAddress = function(xPrivKey,count,cb){
-		var i = 0;
-		var y = 0;
+	root.getAddress = function(xPrivKey, index, path, callback){
 		var bitcore = require('bitcore');
 		var hdPrivateKey = bitcore.HDPrivateKey(xPrivKey);
+
 		// private key derivation
-		var derivedHdPrivateKey = hdPrivateKey.derive("m/45'/2147483647/0/"+count);
+		var derivedHdPrivateKey = hdPrivateKey.derive(path + index);
 		var derivedPrivateKey = derivedHdPrivateKey.privateKey;
+
 		// public key derivation
 		var derivedHdPublicKey = derivedHdPrivateKey.hdPublicKey;
 		var derivedPublicKey = derivedHdPublicKey.publicKey;
-		var address = new bitcore.Address([derivedPublicKey],1);
-		//console.log(address.toString());
-		root.isAddr(address.toString()).then(function(response){
-			var ret;		
-			if(response.data.unconfirmedTxApperances + response.data.txApperances > 0){
-				ret=address.toString();
-			}
-			//console.log(response);
-			     return cb(ret);
-			});
 
-		// var derivedHdPrivateKey = hdPrivateKey.derive("m/45'/2147483647/1/"+count);
-		// var derivedPrivateKey = derivedHdPrivateKey.privateKey;
-		// // public key derivation
-		// var derivedHdPublicKey = derivedHdPrivateKey.hdPublicKey;
-		// var derivedPublicKey = derivedHdPublicKey.publicKey;
-		// var address = new bitcore.Address([derivedPublicKey],1);
-		// //console.log(address.toString());
-		// isAddr(address.toString()).then(function(response){		
-		// 	if(response.data.unconfirmedTxApperances + response.data.txApperances > 0){
-		// 		addr.push(' '+address.toString());
-		// 	}
-		// 	//console.log(response);
-		// 	return cb(addr);
-		// 	});
+		// p2sh 1-1
+		var address = new bitcore.Address([derivedPublicKey],1);
+
+		root.isAddr(address.toString())
+		.then(function(response){
+			var ret;
+
+			if(response.data.unconfirmedTxApperances + response.data.txApperances > 0)
+				ret = address.toString();
+			
+			return callback(ret);
+		});
 	}
 	
 	root.isAddr = function (address){
-			return $http.get('https://test-insight.bitpay.com/api/addr/'+address+'?noTxList=1');
+			return $http.get('https://test-insight.bitpay.com/api/addr/' + address + '?noTxList=1');
 	};
 
-	root.getActiveAddresses = function(pwd,bck,cb){
+	root.getActiveAddresses = function(password, backUp, path, callback){
+		var xPrivKey = root.getXPrivKey(password, backUp);
+		var inactiveCount = 0;
+		var count = 0;
+		var ret = [];
 
-			var xPrivKey = root.getXPrivKey(pwd,bck);
-			var inactiveCount=0;
-			var count=0;
-			var ret=[];
+		function derive(index) {
+			root.getAddress(xPrivKey, index, path, function(addr) {		
+				if (addr) {
+					ret.push(addr);
+					inactiveCount = 0;
+				}
+				else 
+					inactiveCount++;
 
-			function derive(index) {
-				root.getAddress(xPrivKey, index, function(addr) {		
-					if (addr) {
-						ret.push(addr);
-						inactiveCount=0;
-					}
-					else 
-						inactiveCount++;
-
-					if (inactiveCount > GAP ){
-						return cb(ret);}
-					else 
-						derive(index +1);
-				});
-		    };
-			derive(0);
+				if (inactiveCount > GAP)
+					return callback(ret);
+				else 
+					derive(index +1);
+			});
+	    };
+		derive(0);
 	}
 
 	return root;
