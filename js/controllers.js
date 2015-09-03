7a38b62ac328048aece972f5da20a0cb34e75900
Javier <javier@bitpay.com>
2015-09-03 13:26:33 -0300

    adding output properties

--- js/controllers.js
+++ js/controllers.js
@@ -8,23 +8,48 @@ app.controller("addressGeneratorController",function($scope, generatorServices){
 		var changePath = "m/45'/2147483647/1/";
 		$scope.textArea = "";
 		
-		$scope.textArea = 'Searching addresses...' + '\n';
+		$scope.textArea = 'Searching main addresses...' + '\n\n';
 
 		if(validation == true){
 			// getting main addresses
-			generatorServices.getActiveAddresses(password, backUp, mainPath, function(mainAddresses) {
-				if(mainAddresses.length > 0)
-					$scope.textArea += 'Main addresses:\n' + mainAddresses.toString().replace(/,/g,'\n') + '\n\n';
+			generatorServices.getActiveAddresses(password, backUp, mainPath, function(mainAddressesObject) {
+				if(mainAddressesObject.length > 0){
+					$scope.textArea += 'Addresses found: ' + mainAddressesObject.length + '\n' +
+										'*************************' + '\n';
+					
+					for(var i=0; i<mainAddressesObject.length ;i++){
+						$scope.textArea += 'Address: ' + mainAddressesObject[i].address + '\n';
+						$scope.textArea += 'Balance: ' + mainAddressesObject[i].balance + '\n';
+						$scope.textArea += 'Path: ' + mainAddressesObject[i].path + '\n';
+						$scope.textArea += 'Utxos: ' + mainAddressesObject[i].utxo + '\n\n';
+						console.log(mainAddressesObject);
+					}
+				}
 				else
-					$scope.textArea += 'No Main addresses available.' + '\n';
-			});
+					$scope.textArea += 'No Main addresses available.' + '\n\n';
 
-			// gettin change addresses
-			generatorServices.getActiveAddresses(password, backUp, changePath, function(changeAddresses) {
-				if(changeAddresses.length > 0)
-					$scope.textArea += 'Change addresses:\n' + changeAddresses + '\n\n';
-				else
-					$scope.textArea += 'No CHange addresses available.' + '\n';
+				$scope.textArea += '------------------------------------------------------------------------------------------------------' + 
+									'-----------------------------------------------------------------------------------------------------' + 
+									'\nSearching change addresses...' + '\n\n';
+				
+				// gettin change addresses
+				generatorServices.getActiveAddresses(password, backUp, changePath, function(changeAddressesObject) {
+
+					if(changeAddressesObject.length > 0){
+						$scope.textArea += 'Addresses found: ' + changeAddressesObject.length + '\n' +
+											'*************************' + '\n';
+
+						for(var n=0; n<changeAddressesObject.length ;n++){
+							$scope.textArea += 'Address: ' + changeAddressesObject[n].address + '\n';
+							$scope.textArea += 'Balance: ' + changeAddressesObject[n].balance + '\n';
+							$scope.textArea += 'Path: ' + changeAddressesObject[n].path + '\n';
+							$scope.textArea += 'Utxos: ' + changeAddressesObject[n].utxo + '\n\n';
+							console.log(changeAddressesObject);
+						}
+					}
+					else
+						$scope.textArea += 'No Change addresses available.' + '\n\n';
+				});
 			});
 		}
 		else{
--- js/services.js
+++ js/services.js
@@ -1,33 +1,34 @@
 var app = angular.module("addressGeneratorApp.services",[]);
 app.service('generatorServices',['$http',function($http){
-var i = 0;
-var GAP = 20;
-var root = {};
+	var bitcore = require('bitcore');
+	var GAP = 20;
+	var root = {};
 
 	root.validate = function(backUp, password){
-		if (backUp == "" || password == "") {
+		if (backUp == "" || password == "")
 			return "Please Enter values for all entry boxes.";
-		};
+
 		try {
-			jQuery.parseJSON( backUp );
+			jQuery.parseJSON(backUp);
 		} catch(e) {
 			return "Your JSON is not valid, please copy only the text within (and including) the { } brackets around it.";
 		};
+
 		var plain;
 		try {
 			plain = sjcl.decrypt(password, backUp);
 		} catch(e) {
 			return "Seems like your password is incorrect. Try again.";
 		};
+
 		return true;
-	};
+	}
 
 	root.getXPrivKey = function(password, backUp){
 		return JSON.parse(sjcl.decrypt(password, backUp).toString()).xPrivKey;
-	};
+	}
 
 	root.getAddress = function(xPrivKey, index, path, callback){
-		var bitcore = require('bitcore');
 		var hdPrivateKey = bitcore.HDPrivateKey(xPrivKey);
 
 		// private key derivation
@@ -41,43 +42,78 @@ var root = {};
 		// p2sh 1-1
 		var address = new bitcore.Address([derivedPublicKey],1);
 
+		// call insight API for get address information
 		root.isAddr(address.toString())
-		.then(function(response){
-			var ret;
+		.then(function(responseAddress){
 
-			if(response.data.unconfirmedTxApperances + response.data.txApperances > 0)
-				ret = address.toString();
-			
-			return callback(ret);
+			// call insight API for get utxo information
+			root.isUtxo(address.toString())
+			.then(function(responseUtxo){
+
+				var retObject = {};
+				var totalAmount = 0;
+
+				for (var i=0; i<responseUtxo.data.length ; i++)
+                    totalAmount += responseUtxo.data[i].amount;
+
+				if(responseAddress.data.unconfirmedTxApperances + responseAddress.data.txApperances > 0)
+					retObject = {address: responseAddress.data.addrStr, 
+								balance: responseAddress.data.balance,
+								path: path,
+								utxo: totalAmount};
+
+				return callback(retObject);
+			});
 		});
 	}
 	
 	root.isAddr = function (address){
-			return $http.get('https://test-insight.bitpay.com/api/addr/' + address + '?noTxList=1');
-	};
+		return $http.get('https://test-insight.bitpay.com/api/addr/' + address + '?noTxList=1');
+	}
 
-	root.getActiveAddresses = function(password, backUp, path, callback){
+	root.isUtxo = function (address){
+	    return $http.get('https://test-insight.bitpay.com/api/addr/' + address + '/utxo?noCache=1');
+    }
+
+	root.getActiveAddresses = function (password, backUp, path, callback){
 		var xPrivKey = root.getXPrivKey(password, backUp);
 		var inactiveCount = 0;
 		var count = 0;
-		var ret = [];
+		var retObject_ = [];
 
-		function derive(index) {
-			root.getAddress(xPrivKey, index, path, function(addr) {		
-				if (addr) {
-					ret.push(addr);
+		function derive (index) {
+			root.getAddress(xPrivKey, index, path, function(retObject) {		
+				if (!jQuery.isEmptyObject(retObject)) {
+					retObject_.push(retObject);
 					inactiveCount = 0;
 				}
 				else 
 					inactiveCount++;
 
 				if (inactiveCount > GAP)
-					return callback(ret);
+					return callback(retObject_);
 				else 
 					derive(index +1);
 			});
 	    };
 		derive(0);
+	}
+
+	root.createTx = function (txData){
+		var Transaction = bitcore.Transaction;
+
+	//  .from(utxos)          	Feed information about what unspent outputs one can use
+	// 	.to(address, amount) 	Add an output with the given amount of satoshis
+	// 	.change(address)      	Sets up a change address where the rest of the funds will go
+	// 	.sign(privkeySet)     	Signs all the inputs it can
+
+		var tx = new Transaction();
+		tx.from();
+		tx.to();
+		tx.change();
+		tx.sign();
+
+		var serialized = tx.toObject();
 	}
 
 	return root;
