5a61ad6a82f7118380804806b149590a327bc8a3
Gabriel Bazán <gabrielbazan7@hotmail.com>
2015-09-08 15:12:46 -0300

    multisig address

--- js/services.js
+++ js/services.js
@@ -6,49 +6,49 @@ app.service('generatorServices',['$http', 'lodash',function($http, lodash){
 	var GAP = 20;
 	var root = {};
 
-	root.valGenerate = function(backUp, password){
-		for (var i = backUp.length - 1; i >= 0; i--) {
-			if (backUp[i] == "" || password[i] == "")
-			return "Please enter values for all entry boxes.";
+	 root.valGenerate = function(backUps, passwords, m, n){
+        for (var i = backUps.length - 1; i >= 0; i--) {
+            if (backUps[i] == "" || passwords[i] == "")
+                return "Please enter values for all entry boxes.";
 
-		try {
-			jQuery.parseJSON(JSON.parse(backUp[i]));
-		} catch(e) {
-			return "Your JSON is not valid, please copy only the text within (and including) the { } brackets around it.";
-		};
+            try {
+                jQuery.parseJSON(backUps[i]);
+            } catch(e) {
+                return "Your JSON is not valid, please copy only the text within (and including) the { } brackets around it.";
+            };
 
-		var plain;
-		try {
-			plain = sjcl.decrypt(password[i], JSON.parse(backUp[i]));
-		} catch(e) {
-			return "Seems like your password is incorrect. Try again.";
-		};
-		}
-		return true;
-	}
+            try {
+                sjcl.decrypt(passwords[i], backUps[i]);
+            } catch(e) {
+                return "Seems like your password is incorrect. Try again.";
+            };
+
+            if (JSON.parse(sjcl.decrypt(passwords[i], backUps[i])).m != m || JSON.parse(sjcl.decrypt(passwords[i], backUps[i])).n != n)
+                return "The wallet (" + i + ") type (m/n) is not matched with 'm' and 'n' values.";
+        }
+        return true;
+    }
 
 	root.getXPrivKey = function(password, backUp){
-		console.log(JSON.parse(backUp));
-		console.log(password);
-		console.log(sjcl.decrypt(password, JSON.parse(backUp)));
-		console.log("hola2");
-		console.log(sjcl.decrypt(password, JSON.parse(backUp)));
-		return JSON.parse(sjcl.decrypt(password, JSON.parse(backUp)).toString()).xPrivKey;
+		return JSON.parse(sjcl.decrypt(password, backUp).toString()).xPrivKey;
 	}
 
-	root.getAddress = function(xPrivKeys, index, path, callback){
+	root.getAddress = function(xPrivKeys, index, path, n, callback){
 		var derivedPublicKeys = [];
+		var derivedPrivateKeys =[];
+
 		for (var i = xPrivKeys.length - 1; i >= 0; i--) {
 			var hdPrivateKey = bitcore.HDPrivateKey(xPrivKeys[i]);
 			// private key derivation
 			var derivedHdPrivateKey = hdPrivateKey.derive(path + index);
 			var derivedPrivateKey = derivedHdPrivateKey.privateKey;
-
+			derivedPrivateKeys.push(derivedPrivateKey);
 			// public key derivation
 			var derivedHdPublicKey = derivedHdPrivateKey.hdPublicKey;
 			derivedPublicKeys.push(derivedHdPublicKey.publicKey);
 		}
-		var address = new bitcore.Address(derivedPublicKeys,xPrivKeys.length);
+		var address = new bitcore.Address(derivedPublicKeys,n);
+
 		// call insight API for get address information
 		root.isAddr(address.toString())
 		.then(function(responseAddress){
@@ -58,17 +58,20 @@ app.service('generatorServices',['$http', 'lodash',function($http, lodash){
 			.then(function(responseUtxo){
 				var privKeys = [];
 				var retObject = {};
-
+				var utxos = {};
+				var privKeyArray = [];
 				if(responseAddress.data.unconfirmedTxApperances + responseAddress.data.txApperances > 0){
-					// for(var i=0; i<responseUtxo.data.length ;i++)
-					lodash.each(responseUtxo.data)
-						privKeys.push(derivedPrivateKey);
-
+						for (var j = 0; j<derivedPrivateKeys.length; j++) {
+						    privKeys.push(derivedPrivateKeys[j]);}
+							for(var i=0; i<responseUtxo.data.length ;i++) {
+						privKeyArray.push(privKeys);
+					}
+					 utxos = {utxo: responseUtxo.data,
+					 		 privKeys: privKeyArray};
 					retObject = {address: responseAddress.data.addrStr, 
 								balance: responseAddress.data.balance,
 								path: path + index,
-								utxo: responseUtxo.data,
-								privKey: privKeys};
+								utxoData: utxos};
 				}
 
 				return callback(retObject);
@@ -84,19 +87,17 @@ app.service('generatorServices',['$http', 'lodash',function($http, lodash){
 	    return $http.get('https://test-insight.bitpay.com/api/addr/' + address + '/utxo?noCache=1');
     }
 
-	root.getActiveAddresses = function(passwords, backUps, path, callback){
+	root.getActiveAddresses = function(passwords, backUps, path, n, callback){
 		var xPrivKeys = [];
 		for (var i = 0; i < backUps.length ; i++) {
-			console.log(JSON.parse(backUps[i]));
-			xPrivKeys.push(root.getXPrivKey(passwords[i], JSON.parse(backUps[i])));
+			xPrivKeys.push(root.getXPrivKey(passwords[i], backUps[i]));
 		}
-		console.log(xPrivKeys);
 		var inactiveCount = 0;
 		var count = 0;
 		var retObject_ = [];
 
 		function derive(index){
-			root.getAddress(xPrivKeys, index, path, function(retObject) {		
+			root.getAddress(xPrivKeys, index, path, n, function(retObject) {		
 				if (!jQuery.isEmptyObject(retObject)) {
 					retObject_.push(retObject);
 					inactiveCount = 0;
@@ -113,17 +114,22 @@ app.service('generatorServices',['$http', 'lodash',function($http, lodash){
 		derive(0);
 	}
 
-	root.createRawTx = function(address, utxos, totalBalance, privKeys){
+	root.createRawTx = function(address, utxos, totalBalance, privKeys, n){
+		var pub = [];
+		var pri = [];
 		var tx = new Transaction();
 		var amount = parseInt((totalBalance * 100000000 - 10000).toFixed(0));
-
-		for(var i=0; i<utxos.length ;i++){
-			var priv = new bitcore.PrivateKey(privKeys[i]);
-			tx.from(utxos[i], [priv.publicKey], 1);
+		for(var j=0; j<utxos.length ;j++){
+			for (var i = 0; i<privKeys[j].length; i++) {
+				var priv = new bitcore.PrivateKey(privKeys[j][i]);
+				pri.push(priv);
+				pub.push(priv.publicKey);
+			}
+			tx.from(utxos[j], pub, n);
+			pub=[];
 		}
-
 		tx.to(address, amount);
-		tx.sign(lodash.uniq(privKeys));
+		tx.sign(lodash.uniq(pri));
 
 		var rawTx = tx.serialize();
 		return rawTx;
